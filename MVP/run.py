6489f801d67839ff147fb51ce453f2d0d81cf5cc
CaioDBP <caaio.p13@gmail.com>
2025-11-17 15:42:42 -0300

    Mudando o nome do sistema entre outras coisas

--- MVP/run.py
+++ MVP/run.py
@@ -2,11 +2,11 @@ from app import create_app
 
 app = create_app()
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
-import os
+# import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(debug=True, host="0.0.0.0", port=port)
